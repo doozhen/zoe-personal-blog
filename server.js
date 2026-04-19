6493681c44f556977a678b6cbd4eb8c507a4d617
doozhen <1410478022@qq.com>
2026-04-19 22:48:49 +0800

    添加验证页面，保护博客访问

--- server.js
+++ server.js
@@ -16,6 +16,10 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(express.static('public'));
 
+app.get('/', (req, res) => {
+    res.redirect('/verify.html');
+});
+
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
